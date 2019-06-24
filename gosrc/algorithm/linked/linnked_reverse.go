53b07a0932e673a20bcede60e9c973074a617d23
longlong202 <ustblonglong@sina.com>
2019-06-24 09:28:02 +0800

    insert linked

--- gosrc/algorithm/linked/linnked_reverse.go
+++ gosrc/algorithm/linked/linnked_reverse.go
@@ -52,12 +52,46 @@ func RecursiveReverse(node *LNode)  {
 	node.Next = newHead
 }
 
+//方法三 插入法
+//只需要主线的next指针和cur当前指针即可
+//head->1->2->3->4
+//从2开始处理，即初始化的时候,cur代表当前要处理的元素,所以cur = node.Next.Next
+//因为结尾最终1是尾指针，所以node.Next.Next = nil
+/*
+next
+cur
+要将head->1->2->3->4 第一步处理之后变成head->2->1 3->4
+将2->1之前，需要将2->3的地址3记录下来，cur是2，则next是3，用next指针记录下一步轮询要处理的指针地址，记录好2->3的指针后，可以断开2->3，变成2->1
+ */
+func InsertReverse( node *LNode)  {
+	if node == nil || node.Next ==nil {
+		return
+	}
+
+	var cur *LNode //当前看结点指针
+	var next *LNode //后继结点指针
+	cur = node.Next.Next
+
+	//设置链表第一个结点为尾结点
+	node.Next.Next = nil //1的后继指针将是nil
+	//把遍历到的结点插入到头结点后面
+	for cur != nil {
+		next = cur.Next //记录next指针，下一次轮询用
+		cur.Next = node.Next //将2->3的指针变成2->1
+		node.Next = cur //将head->1的指针变成 head->2 插入头结点的后面
+		cur = next //将当前指针移到next指针准备下一次轮询
+	}
+
+}
+
+
 func main()  {
 	head := &LNode{}
 	fmt.Println("就地逆序")
 	CreateNode(head,8)
 	PrintNode("逆序前：",head)
 	//Reverse(head) //方法一
-	RecursiveReverse(head)//方法二
+	//RecursiveReverse(head)//方法二
+	InsertReverse(head)//方法三
 	PrintNode("逆序后:",head)
 }
